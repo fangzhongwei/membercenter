8499c582aca2f68fa6c56a22ba4aadcc40265702
fangzhongwei <zhongwei.fang@56qq.com>
2016-10-27 15:30:45 +0800

    member center

--- src/main/scala/com/lawsofnature/rpc/MemberServer.scala
+++
@@ -1,8 +0,0 @@
-package com.lawsofnature.rpc
-
-/**
-  * Created by fangzhongwei on 2016/10/11.
-  */
-trait MemberServer {
-  def initServer
-}
--- src/main/scala/com/lawsofnature/service/RpcService.scala
+++ src/main/scala/com/lawsofnature/service/RpcService.scala
@@ -5,41 +5,24 @@ import java.util
 import Ice.ObjectImpl
 import com.google.inject.name.Names
 import com.google.inject.{AbstractModule, Guice}
-import com.lawsofnatrue.common.ice.{IceServerTemplate, IceServerTemplateImpl}
+import com.lawsofnatrue.common.ice.{ConfigHelper, IceServerTemplate, IceServerTemplateImpl}
 import com.lawsofnature.repo._
-import com.lawsofnature.rpc.{MemberEndpointImpl, MemberServer, MemberServerImpl}
-import com.typesafe.config.{Config, ConfigFactory, ConfigRenderOptions}
+import com.lawsofnature.rpc.MemberEndpointImpl
 import org.slf4j.LoggerFactory
 
-import scala.collection.JavaConversions._
 
 object RpcService extends App {
   var logger = LoggerFactory.getLogger(this.getClass)
 
   private val injector = Guice.createInjector(new AbstractModule() {
     override def configure() {
-      val map: util.HashMap[String, String] = configMap
+      val map: util.HashMap[String, String] = ConfigHelper.configMap
       Names.bindProperties(binder(), map)
       bind(classOf[MemberRepository]).to(classOf[MemberRepositoryImpl]).asEagerSingleton()
       bind(classOf[ObjectImpl]).to(classOf[MemberEndpointImpl]).asEagerSingleton()
-      bind(classOf[MemberServer]).to(classOf[MemberServerImpl]).asEagerSingleton()
       bind(classOf[IceServerTemplate]).to(classOf[IceServerTemplateImpl]).asEagerSingleton()
     }
   })
 
-  def configMap: util.HashMap[String, String] = {
-    val config: Config = ConfigFactory.load()
-    val map: util.HashMap[String, String] = new java.util.HashMap[String, String]()
-    for (entry <- config.entrySet()) {
-      val value: String = entry.getValue.render()
-      if (value.length > 2) {
-
-        map.put(entry.getKey, value.substring(1, value.length - 1))
-        println("key:" + entry.getKey + ",value:" + value.substring(1, value.length - 1) + "")
-      }
-    }
-    map
-  }
-
-  injector.getInstance(classOf[MemberServer]).initServer
+  injector.getInstance(classOf[IceServerTemplate]).startServer
 }
