db5bad86b20cbd7b8294ce434acc3b8f5ce998ac
fangzhongwei <zhongwei.fang@56qq.com>
2017-02-02 14:09:20 +0800

    ice to thrift

--- memberserver/src/main/scala/com/jxjxgo/service/RpcService.scala
+++ memberserver/src/main/scala/com/jxjxgo/service/RpcService.scala
@@ -3,15 +3,15 @@ package com.jxjxgo.service
 import java.util
 
 import com.google.inject.name.Names
-import com.google.inject.{AbstractModule, Guice}
+import com.google.inject.{AbstractModule, Guice, TypeLiteral}
 import com.jxjxgo.account.rpc.domain.AccountEndpoint
 import com.jxjxgo.common.helper.ConfigHelper
 import com.jxjxgo.edcenter.rpc.domain.EdServiceEndpoint
-import com.jxjxgo.memberber.rpc.domain.MemberEndpoint
 import com.jxjxgo.repo.{MemberRepository, MemberRepositoryImpl}
 import com.jxjxgo.rpc.MemberEndpointImpl
 import com.jxjxgo.scrooge.thrift.template.{ScroogeThriftServerTemplate, ScroogeThriftServerTemplateImpl}
 import com.twitter.finagle.Thrift
+import com.twitter.scrooge.ThriftService
 import com.twitter.util.Future
 import com.typesafe.config.{Config, ConfigFactory}
 
@@ -20,13 +20,15 @@ object RpcService extends App {
     override def configure() {
       val map: util.HashMap[String, String] = ConfigHelper.configMap
       Names.bindProperties(binder(), map)
+
+      val config: Config = ConfigFactory.load()
+      bind(new TypeLiteral[EdServiceEndpoint[Future]](){}).toInstance(Thrift.client.newIface[EdServiceEndpoint[Future]](config.getString("edcenter.thrift.host.port")))
+      bind(new TypeLiteral[AccountEndpoint[Future]](){}).toInstance(Thrift.client.newIface[AccountEndpoint[Future]](config.getString("account.thrift.host.port")))
       bind(classOf[MemberRepository]).to(classOf[MemberRepositoryImpl]).asEagerSingleton()
       bind(classOf[MemberService]).to(classOf[MemberServiceImpl]).asEagerSingleton()
-      bind(classOf[MemberEndpoint[Future]]).to(classOf[MemberEndpointImpl]).asEagerSingleton()
-      bind(classOf[ScroogeThriftServerTemplate]).to(classOf[ScroogeThriftServerTemplateImpl[MemberEndpoint[Future]]]).asEagerSingleton()
-      val config: Config = ConfigFactory.load()
-      bind(classOf[EdServiceEndpoint[Future]]).toInstance(Thrift.client.newIface[EdServiceEndpoint[Future]](config.getString("edcenter.thrift.host.port")))
-      bind(classOf[AccountEndpoint[Future]]).toInstance(Thrift.client.newIface[AccountEndpoint[Future]](config.getString("account.thrift.host.port")))
+      bind(classOf[ThriftService]).to(classOf[MemberEndpointImpl]).asEagerSingleton()
+      bind(classOf[ScroogeThriftServerTemplate]).to(classOf[ScroogeThriftServerTemplateImpl]).asEagerSingleton()
+
     }
   })
 
